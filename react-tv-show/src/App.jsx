488f6c352a71535cb18c9d8c77f2227dbe62946a
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-02-03 23:34:53 +0100

    Buscador de filmes e series

---
+++ react-tv-show/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logo from "./assets/images/logo.png"
+
+
+export function App() {
+    const [currentTVShow, setCurrentTVShow] = useState();
+
+    async function fetchPopulars(){
+        const populars = await TVShowAPI.fetchPopulars();
+        if(populars.length > 0){
+            setCurrentTVShow(populars[0])
+        }
+    }
+
+    useEffect(()=>{
+        fetchPopulars();
+    },[])
+
+    console.log(currentTVShow)
+
+  return (
+    <div className={s.main_container}
+    style={{background: currentTVShow ?  `linear-gradient(rgba(0,0,0,0.55),rgba(0,0,0,0.55)),url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`: "black",}}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo image={logo} title="Wato Watch" subtitle="Find a show you may like"/>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <input style={{width: '100%',marginTop:"40px", backgroundColor: "transparent", border: "1px solid #fff",color: "#00d3d2"}} type="text"/>
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>{currentTVShow && <TVShowDetail tvShow={currentTVShow}/>}</div>
+      <div className={s.recommentations}>recommentations</div>
+    </div>
+  );
+}
---
+++ react-tv-show/src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=18b18639eb98c8069b21b7992e27c32f";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL};
